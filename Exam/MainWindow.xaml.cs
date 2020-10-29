4c08af24a3654eb22c67b54c2167643edab1cf60
soniafedorchuk123@gmail.com <soniafedorchuk123@gmail.com>
2020-10-29 20:42:35 +0200

    додавання логіки до кніпочки входу

--- Exam/MainWindow.xaml.cs
+++ Exam/MainWindow.xaml.cs
@@ -20,9 +20,15 @@ namespace Exam
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Your login : {txtLogin.Text}\nYour password : {txtPassword.Text}");
         }
     }
 }
